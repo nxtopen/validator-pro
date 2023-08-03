971c3759300f6f74ea586417ac5e39d471bfd8d9
gilzonme <gilzon.me@gmail.com>
2023-08-03 21:55:59 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// Yet to start
+const fs = require('fs');
+
+class ValidatorPro {
+    constructor() {
+        this.regexMap = {};
+        this.loadRegexFromJson();
+    }
+
+    loadRegexFromJson() {
+        const filePath = __dirname + '/regex.json';
+        const jsonData = fs.readFileSync(filePath, 'utf-8');
+        this.regexMap = JSON.parse(jsonData);
+    }
+
+    validate(type, inputString) {
+        const regexPattern = this.regexMap[type];
+        if (!regexPattern) {
+            throw new Error(`Invalid type '${type}'.`);
+        }
+
+        const regex = new RegExp(regexPattern);
+        return regex.test(inputString);
+    }
+
+    escape(type, inputString) {
+        const regexPattern = this.regexMap[type];
+        if (!regexPattern) {
+            throw new Error(`Invalid type '${type}'.`);
+        }
+
+        const regex = new RegExp(`[^${regexPattern}]`, 'g');
+        return inputString.replace(regex, '');
+    }
+}
+
+module.exports = ValidatorPro;
